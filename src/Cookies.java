68055381660f2fcdf4d5169fbcf782a634c3800e
TT0l23 <thaye.lagrisola@ht-la.org>
2026-06-02 22:14:08 -0700

    Cookies.java

--- src/Cookies.java
+++ src/Cookies.java
@@ -4,13 +4,17 @@ import java.util.Timer;
 public class Cookies {
     int cookieCount = 0;
     int AmountPerClick = 1;
+    int AmountPerClick2 = 2;
     int Upgrade1Purchases = 0;
-    int Upgrade1BaseCost = 15;
+    int Upgrade2Purchases = 0;
+    int Upgrade1BaseCost = 10;
+    int Upgrade2BaseCost = 50;
     Timer CPSTimer = new Timer();
 
     public int getCookieCount(){return cookieCount;}
     public int getAmountPerClick(){return AmountPerClick;}
     public int getUpgrade1Purchases(){return Upgrade1Purchases;}
+    public int getUpgrade2Purchases(){return Upgrade2Purchases;}
 
     public void AutoTimer() {
         CPSTimer.schedule(new TimerTask() {
@@ -25,9 +29,14 @@ public class Cookies {
         cookieCount+=AmountPerClick;
     }
 
+
     public int getUpgrade1Cost(){
-        //15(base cost)*(1.15)^(times purchased)
-        return (int) Math.round(Upgrade1BaseCost*Math.pow(1.15,Upgrade1Purchases));
+        //15(base cost)*(1.10)^(times purchased)
+        return (int) Math.round(Upgrade1BaseCost*Math.pow(1.10,Upgrade1Purchases));
+    }
+    public int getUpgrade2Cost(){
+        //15(base cost)*(1.50)^(times purchased)
+        return (int) Math.round(Upgrade2BaseCost*Math.pow(1.50,Upgrade2Purchases));
     }
 
     public boolean buyUpgrade1(){
@@ -36,6 +45,16 @@ public class Cookies {
             cookieCount-=cost;
             Upgrade1Purchases++;
             AmountPerClick++;
+            return true;
+        }
+        return false;
+    }
+    public boolean buyUpgrade2(){
+        int cost = getUpgrade2Cost();
+        if(cookieCount>=cost){
+            cookieCount-=cost;
+            Upgrade2Purchases++;
+            AmountPerClick2++;
             return true;
         }
         return false;
